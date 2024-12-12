b9b5dc643ca1268ce8ef5b4f7484df1cbb7fcb80
Franzero <onepagesword@gmail.com>
2024-12-12 17:18:01 +0800

    Fixed: build error in Win

--- test/src/examples/benchmark.cpp
+++ test/src/examples/benchmark.cpp
@@ -1,7 +1,3 @@
-#include <cstdio>
-#include <unistd.h>
-
-#include <filesystem>
 #include <iostream>
 #include <memory>
 #include <xtensor/xnpy.hpp>
@@ -17,22 +13,6 @@
 #include "xfdtd/simulation/simulation.h"
 
 void dielectricSphereScatter(int argc, char* argv[]) {
-  // auto cpus = sysconf(_SC_NPROCESSORS_ONLN);
-  // printf("cpu: %ld\n", cpus);
-
-  // int cpu_id = 4;
-  // cpu_set_t mask;
-  // CPU_ZERO(&mask);
-  // CPU_SET(cpu_id, &mask);
-  // if (sched_setaffinity(0, sizeof(mask), &mask) == -1) {
-  //   printf("Could not set CPU affinity\n");
-  // } else {
-  //   printf("CPU affinity set to CPU %d\n", cpu_id);
-  // }
-  // press any key to continue
-  // std::cin.get();
-  // xfdtd::MpiSupport::setMpiParallelDim(2, 2, 1);
-
   auto program = argparse::ArgumentParser("dielectricSphereScatter");
   program.add_argument("-t", "--time_steps")
       .help("Number of time steps")
@@ -59,7 +39,7 @@ void dielectricSphereScatter(int argc, char* argv[]) {
   try {
     program.parse_args(argc, argv);
   } catch (const std::runtime_error& err) {
-    std::cout << err.what() << std::endl;
+    std::cout << err.what() << '\n';
     std::cout << program;
     exit(0);
   }
@@ -68,15 +48,13 @@ void dielectricSphereScatter(int argc, char* argv[]) {
   auto dl = program.get<double>("-d");
   auto thread_config = program.get<std::vector<int>>("-t_c");
   auto with_pml = program.get<bool>("--with_pml");
-  std::cout << "time_steps: " << time_steps << std::endl;
-  std::cout << "dl: " << dl << std::endl;
+  std::cout << "time_steps: " << time_steps << '\n';
+  std::cout << "dl: " << dl << '\n';
   std::cout << "thread_config: " << thread_config[0] << " " << thread_config[1]
-            << " " << thread_config[2] << std::endl;
-  std::cout << "with_pml: " << std::boolalpha << with_pml << std::endl;
+            << " " << thread_config[2] << '\n';
+  std::cout << "with_pml: " << std::boolalpha << with_pml << '\n';
 
   using namespace std::string_view_literals;
-  constexpr auto data_path_str = "./data/dielectric_sphere_scatter"sv;
-  const auto data_path = std::filesystem::path{data_path_str};
 
   auto domain{std::make_shared<xfdtd::Object>(
       "domain",
